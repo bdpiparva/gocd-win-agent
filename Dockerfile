b0aaf8ac89f2f6d1fb50d6552513d018d1b8dcb2
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2018-07-31 11:28:00 +0530

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM microsoft/windowsservercore
+MAINTAINER GoCD Team <go-cd-dev@googlegroups.com>
+
+COPY user-config C:/Users/ContainerAdministrator/user-config
+COPY *.ps1 C:/
+RUN powershell -File C:\bootstrap.ps1
+RUN powershell -File C:\init-gradle.ps1
+
+CMD C:\\go-agent.exe
---
+++ bootstrap.ps1
@@ -0,0 +1,42 @@
+$GOLANG_BOOTSTRAPPER_VERSION='1.1'
+$P4_VERSION='15.1'
+$P4D_VERSION='16.2'
+$NODEJS_VERSION='6.13.1'
+$RUBY_VERSION='1.9.3.55100'
+$NANT_VERSION='0.92.2'
+
+# install chocolatey
+$chocolateyUseWindowsCompression='false'
+$ErrorActionPreference = "Stop" 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy Bypass
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install packages
+choco install -y nodejs.install --version="${NODEJS_VERSION}"
+choco install -y hg yarn jdk8 svn ant
+choco install -y ruby --version=${RUBY_VERSION}
+choco install -y nant --version=${NANT_VERSION}
+
+# Remove chocolatey
+Remove-Item C:\\Users\\ContainerAdministrator\\AppData\\Local\\Temp\\chocolatey -Force -Recurse
+
+# install p4
+New-Item "${env:ProgramFiles(x86)}\\Perforce\\bin\\" -ItemType Directory
+Invoke-WebRequest https://s3.amazonaws.com/mirrors-archive/local/perforce/r$P4_VERSION/bin.ntx64/p4.exe -Outfile "C:\\Program Files (x86)\\Perforce\\bin\\p4.exe"
+Invoke-WebRequest https://s3.amazonaws.com/mirrors-archive/local/perforce/r$P4D_VERSION/bin.ntx64/p4d.exe -Outfile "C:\\Program Files (x86)\\Perforce\\bin\\p4d.exe"
+
+# install gocd bootstrapper
+Invoke-WebRequest https://github.com/ketan/gocd-golang-bootstrapper/releases/download/${GOLANG_BOOTSTRAPPER_VERSION}/go-bootstrapper-${GOLANG_BOOTSTRAPPER_VERSION}.windows.amd64.exe -Outfile C:\\go-agent.exe
+
+# install git
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.18.0.windows.1/MinGit-2.18.0-busybox-64-bit.zip -OutFile C:\\git.zip
+Expand-Archive C:\\git.zip C:\\git
+Remove-Item -Path C:\\git.zip
+$newSystemPath = [System.Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
+$newSystemPath = "${newSystemPath};${env:ProgramFiles(x86)}\\Perforce\\bin;C:\\Git\\Cmd;C:\tools\ruby193\bin\;${env:ProgramFiles(x86)}\nant-0.92\bin;"
+[Environment]::SetEnvironmentVariable("Path", $newSystemPath, [EnvironmentVariableTarget]::Machine)
+$env:Path = $newSystemPath + ";" + [System.Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User)
+
+Add-LocalGroupMember -Group "Administrators" -Member "ContainerAdministrator"
+npm config set msvs_version 2015
